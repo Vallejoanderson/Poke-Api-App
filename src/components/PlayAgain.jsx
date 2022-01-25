4dd8fee6014380e92a7185a387c20142801dfae3
Anderson Vallejo <vallejoanderson@gmail.com>
2022-01-25 14:23:04 -0500

    Play Button width corrected

--- src/components/PlayAgain.jsx
+++ src/components/PlayAgain.jsx
@@ -10,7 +10,7 @@ export const PlayAgain = () => {
 	
 	return <button
 					className="bg-rose-800 text-slate-100 py-3 px-20 rounded-lg mx-auto
-										 flex justify-center w-3/4 font-bold tracking-widest w-96" 
+										 flex justify-center font-bold tracking-widest w-96" 
 					onClick = { () => { setHearts( 3 ); setRound( 1 ) } }
 				 >
 					PLAY AGAIN
